ee3d7e0204ce6c1d95d39ab183e9bd9eddb17597
Jan Rubel <jasiekrubel@gmail.com>
2020-10-28 18:54:26 +0100

    Add models and register them to admin

--- kronikarz/api/admin.py
+++ kronikarz/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import (
+    Event,
+    FamilyTree,
+    Mariage,
+    Media,
+    Person,
+    User
+)
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+
+admin.site.register((Event, FamilyTree, Mariage, Media, Person))
--- kronikarz/api/models.py
+++ kronikarz/api/models.py
@@ -1,3 +1,68 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class FamilyTree(models.Model):
+    user = models.ForeignKey('api.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=10000, null=True)
+
+
+class Person(models.Model):
+    class Sex(models.TextChoices):
+        MALE = 'M', _('Male')
+        FEMALE = 'F', _('Female')
+        OTHER = 'O', _('Other')
+
+    family_tree = models.ForeignKey('api.FamilyTree', on_delete=models.CASCADE)
+    father = models.ForeignKey(
+        'api.Person',  null=True, on_delete=models.SET_NULL, related_name='person_father')
+    mother = models.ForeignKey(
+        'api.Person', null=True, on_delete=models.SET_NULL, related_name='person_mother')
+    name = models.CharField(max_length=50, null=True)
+    surname = models.CharField(max_length=50, null=True)
+    birth_date = models.DateField(null=True)
+    # think about enum or another class, django-countries?
+    nationality = models.CharField(max_length=100, null=True)
+    sex = models.CharField(max_length=6, choices=Sex.choices, null=True)
+    birth_place = models.CharField(max_length=150, null=True)
+    death_date = models.DateField(null=True)
+    death_cause = models.CharField(max_length=200, null=True)
+
+
+class Event(models.Model):
+    class Icons(models.TextChoices):
+        OTHER = 'OTHER', _("Other")
+        # Develop more icons
+
+    person = models.ForeignKey('api.Person', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000, null=True)
+    date = models.DateField(null=True)
+    icon = models.CharField(
+        max_length=20, choices=Icons.choices, default=Icons.OTHER)
+
+
+class Mariage(models.Model):
+    person_1 = models.ForeignKey(
+        'api.Person', related_name='person_1', on_delete=models.CASCADE)
+    person_2 = models.ForeignKey(
+        'api.Person', related_name='person_2', on_delete=models.CASCADE)
+    marriage_date = models.DateField(null=True)
+    divorce_date = models.DateField(null=True)
+
+
+def user_directory_path(instance, filename):
+    return f'user_{instance.user.id}/filename'
+
+
+class Media(models.Model):
+    person = models.ForeignKey('api.Person', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    file = models.FileField(upload_to=user_directory_path)

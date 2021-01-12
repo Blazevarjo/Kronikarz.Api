93c69db6922b4b9f93138bbb46645ff0bf5be0df
Jan Rubel <jasiekrubel@gmail.com>
2021-01-12 18:17:19 +0100

    Add docker support

--- kronikarz/settings.py
+++ kronikarz/settings.py
@@ -130,7 +130,7 @@ DATABASES = {
         'NAME': 'kronikarzdb',
         'USER': 'kronikarz_user',
         'PASSWORD': 'Zaq12wsx',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
